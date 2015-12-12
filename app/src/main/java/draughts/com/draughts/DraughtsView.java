a1a8e516a8f4b69add2a0f25c62fc93ab7c4f232
avinashgundagani46 <avinash.gundagani46@gmail.com>
2015-12-12 22:50:00 +0530

    Drawing of empty table is done.

--- app/src/main/java/draughts/com/draughts/DraughtsView.java
+++ app/src/main/java/draughts/com/draughts/DraughtsView.java
@@ -1,20 +1,33 @@
 package draughts.com.draughts;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 
-public class DraughtsView extends View{
+public class DraughtsView extends View {
     private Context mContext;
-    private Paint redPaint;
+    private int mViewHeight;
+    private int mBoardSize = 8;
+    private int mHeightOfCell;
+    private Paint mRedPaint = new Paint();
+    private Paint mWhitePaint = new Paint();
+    private Paint mBlackStroke = new Paint();
+    private Paint mGreenPaint = new Paint();
+    private Rect rect;
 
     public DraughtsView(Context context) {
         super(context);
         init(context);
     }
+
     public DraughtsView(Context context, AttributeSet attrs) {
         super(context, attrs);
         init(context);
@@ -24,15 +37,43 @@ public class DraughtsView extends View{
         super(context, attrs, defStyleAttr);
         init(context);
     }
+
     private void init(Context context) {
         mContext = context;
-        redPaint = new Paint();
-        redPaint.setColor(Color.RED);
+        WindowManager wm = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        mViewHeight = display.getWidth();
+        mHeightOfCell = mViewHeight / mBoardSize;
+
+        mBlackStroke.setColor(Color.BLACK);
+        mBlackStroke.setStrokeWidth(3);
+        mBlackStroke.setStyle(Paint.Style.STROKE);
+
+        mGreenPaint.setStrokeWidth(3);
+        mGreenPaint.setColor(mContext.getResources().getColor(R.color.colorGreenCell));
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawCircle(100, 100, 500, redPaint);
+        createTable(canvas);
+    }
+
+    /**
+     * Handles table drawing based on board size
+     * @param canvas
+     */
+    private void createTable(Canvas canvas) {
+        for (int i = 0; i < mBoardSize; i++) {
+            for (int j = 0; j < mBoardSize; j++) {
+                if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)) {
+                    rect = new Rect(mHeightOfCell * i, mHeightOfCell * j, mHeightOfCell * i + mHeightOfCell, mHeightOfCell * j + mHeightOfCell);
+                    canvas.drawRect(rect, mGreenPaint);
+                    canvas.drawRect(rect, mBlackStroke);
+                }
+            }
+        }
+        canvas.drawLine(0, mHeightOfCell * mBoardSize, mViewHeight, mHeightOfCell * mBoardSize, mBlackStroke);
     }
 }
